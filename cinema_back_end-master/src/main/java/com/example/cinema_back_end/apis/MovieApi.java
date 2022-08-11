d47f73b37d93e2942488362fefd9061f9c505b91
unknown <hungpham28092003@gmail.com>
2022-08-11 18:45:22 +0700

    first commit

---
+++ cinema_back_end-master/src/main/java/com/example/cinema_back_end/apis/MovieApi.java
@@ -0,0 +1,46 @@
+package com.example.cinema_back_end.apis;
+
+import com.example.cinema_back_end.dtos.MovieDTO;
+import com.example.cinema_back_end.entities.Movie;
+import com.example.cinema_back_end.repositories.IMovieRepository;
+import com.example.cinema_back_end.services.IMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value="/api/movies", produces = "application/json")
+public class MovieApi {
+    @Autowired
+    private IMovieService movieService;
+
+    @Autowired
+    private IMovieRepository movieRepository;
+
+    @GetMapping("/showing")
+    public ResponseEntity<List<MovieDTO>> findAllShowingMovies(){
+        return new ResponseEntity<>(movieService.findAllShowingMovies(), HttpStatus.OK);
+    }
+    @GetMapping("/all-movies")
+    public ResponseEntity<List<MovieDTO>> getAllMovies(){
+        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+    @GetMapping("/details")
+    public MovieDTO getMovieById(@RequestParam Integer movieId){
+        return movieService.getById(movieId);
+    }
+
+    @GetMapping("/showing/search")
+    public List<MovieDTO> findAllShowingMoviesByName(@RequestParam String name){
+        return movieService.findAllShowingMoviesByName(name);
+    }
+
+    @PostMapping
+    public void updateMovie(@RequestBody Movie movie){
+        movieRepository.save(movie);
+    }
+}
